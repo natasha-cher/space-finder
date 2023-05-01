8e79f26a7826d5dbc841732f533a7bd5c6305779
Natalia Chernenko <natchernenko13@gmail.com>
2023-05-01 10:40:51 +0900

    added edit and delete routes

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 const Studio = require('./models/studio');
 main().catch(err => console.log(err));
@@ -35,13 +36,30 @@ app.get('/studios/new', (req, res) => {
   res.render('studios/new')
 })
 
+app.post('/studios', async (req, res) => {
+  const studio = new Studio(req.body.studio);
+  await studio.save();
+  res.redirect(`studios/${ studio._id }`)
+})
+
 app.get('/studios/:id', async (req, res) => {
   const studio = await Studio.findById(req.params.id);
   res.render('studios/show', { studio });
 });
 
-app.post('/studios', async (req, res) => {
-  const studio = new Studio(req.body.studio);
-  await studio.save();
-  res.redirect(`studios/${ studio._id }`)
+app.get('/studios/:id/edit', async (req, res) => {
+  const studio = await Studio.findById(req.params.id);
+  res.render('studios/edit', { studio });
+})
+
+app.put('/studios/:id', async (req, res) => {
+  const { id } = req.params;
+  const studio = await Studio.findByIdAndUpdate(id, { ...req.body.studio });
+  res.redirect(`/studios/${studio._id}`);
+});
+
+app.delete('/studios/:id', async (req, res) => {
+  const { id } = req.params;
+  const studio = await Studio.findByIdAndDelete(id);
+  res.redirect('/studios');
 })
