6660dd47ebb8f267467275a9f89de074e1d30706
Natalia Chernenko <natchernenko13@gmail.com>
2023-05-07 13:29:04 +0900

    adding delete middleware

--- models/studio.js
+++ models/studio.js
@@ -15,4 +15,14 @@ const StudioSchema = new Schema({
   ]
 })
 
+StudioSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+      await Review.deleteMany({
+          _id: {
+              $in: doc.reviews
+          }
+      })
+  }
+})
+
 module.exports = mongoose.model('Studio', StudioSchema)
