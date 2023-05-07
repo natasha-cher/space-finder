86fa8dab1cfe14a354a2853929190e7a42632124
Natalia Chernenko <natchernenko13@gmail.com>
2023-05-07 12:12:37 +0900

    adding the review model

--- models/studio.js
+++ models/studio.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const StudioSchema = new Schema ({
+const StudioSchema = new Schema({
   name: String,
   price: Number,
   description: String,
   location: String,
-  image: String
+  image: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ]
 })
 
 module.exports = mongoose.model('Studio', StudioSchema)
