ae70b8dda28c3da380e32abc455cb627ce28a902
Natalia Chernenko <natchernenko13@gmail.com>
2023-07-13 16:37:37 +0300

    configuring passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
+
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const dotenv = require('dotenv');
@@ -16,6 +17,12 @@ const Joi = require('joi');
 dotenv.config()
 const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
 const sessionSecret = process.env.SESSION_SECRET;
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const Studio = require('./models/studio');
+const Review = require('./models/review');
+const User = require('./models/user');
 
 app.set('views', path.join(__dirname, 'views'));
 
@@ -25,6 +32,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 const sessionConfig = {
   secret: sessionSecret,
@@ -39,14 +47,18 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 })
-
-const Studio = require('./models/studio');
-const Review = require('./models/review');
 
 main().catch(err => console.log(err));
 
@@ -78,5 +90,5 @@ app.use((req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('listening on http://localhost/3000')
+  console.log('listening on http://localhost:3000/')
 });
